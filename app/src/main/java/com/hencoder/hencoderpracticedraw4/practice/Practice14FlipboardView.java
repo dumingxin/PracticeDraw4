3753741affca2c31b628d80a892d80b9482e6ece
dmx <mail@dmx.name>
2017-11-06 18:12:55 +0800

    练习翻页动画

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,8 +20,8 @@ public class Practice14FlipboardView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Camera camera = new Camera();
-    int degree;
-    ObjectAnimator animator = ObjectAnimator.ofInt(this, "degree", 0, 180);
+    int degree=45;
+    ObjectAnimator animator = ObjectAnimator.ofInt(this, "degree", 0, 270);
 
     public Practice14FlipboardView(Context context) {
         super(context);
@@ -40,13 +41,13 @@ public class Practice14FlipboardView extends View {
         animator.setDuration(2500);
         animator.setInterpolator(new LinearInterpolator());
         animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.setRepeatMode(ValueAnimator.RESTART);
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        animator.start();
+//        animator.start();
     }
 
     @Override
@@ -73,25 +74,69 @@ public class Practice14FlipboardView extends View {
         int y = centerY - bitmapHeight / 2;
         //绘制上半部分
         canvas.save();
-        canvas.clipRect(0, 0, getWidth(), centerY);
+        Path path = new Path();
+        if (degree <= 45) {
+            path.moveTo(x + bitmapWidth/2 * (1 - degree / 45f), y);
+            path.lineTo(x, y);
+            path.lineTo(x, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth/2 * (1 - degree / 45f), y+bitmapHeight);
+        } else if (degree <= 135) {
+            path.moveTo(x, y + (degree - 45) / 90f * bitmapHeight);
+            path.lineTo(x, y+bitmapHeight);
+            path.lineTo(x + bitmapWidth, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y - (degree - 45) / 90f * bitmapHeight);
+        } else if (degree <= 225) {
+            path.moveTo(x + (degree - 135) / 90f * bitmapWidth, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y);
+            path.lineTo(x - (degree - 135) / 90f * bitmapWidth, y);
+        } else {
+            path.moveTo(x + bitmapWidth, y + bitmapHeight - (degree - 225) / 45f * bitmapHeight);
+            path.lineTo(x + bitmapWidth, y);
+            path.lineTo(x, y);
+            path.lineTo(x, y + (degree - 225) / 45f * bitmapHeight);
+        }
+
+
+        path.close();
+        canvas.clipPath(path);
         canvas.drawBitmap(bitmap, x, y, paint);
         canvas.restore();
 
         canvas.save();
+        path.reset();
         //绘制下半部分
-        if (degree < 90) {
-            canvas.clipRect(0, centerY, getWidth(), getHeight());
+        if (degree <= 45) {
+            path.moveTo(x + (degree + 45) / 45f * bitmapWidth / 2, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y);
+            path.lineTo(x - (degree + 45) / 45f * bitmapWidth / 2, y);
+        } else if (degree <= 135) {
+            path.moveTo(x + bitmapWidth, y + bitmapHeight - (degree - 225) / 45f * bitmapHeight);
+            path.lineTo(x + bitmapWidth, y);
+            path.lineTo(x, y);
+            path.lineTo(x, y + (degree - 225) / 45f * bitmapHeight);
+        } else if (degree <= 225) {
+            path.moveTo(x, y + (degree - 45) / 90f * bitmapHeight);
+            path.lineTo(x, bitmapHeight);
+            path.lineTo(x + bitmapWidth, y + bitmapHeight);
+            path.lineTo(x + bitmapWidth, y - (degree - 45) / 90f * bitmapHeight);
         } else {
-            canvas.clipRect(0, 0, getWidth(), centerY);
+            path.moveTo(x + centerX * (1 - degree / 45f), y);
+            path.lineTo(x, y);
+            path.lineTo(x, y + bitmapHeight);
+            path.lineTo(x + centerX * (1 - degree / 45f), y);
         }
+        path.close();
+        canvas.clipPath(path);
         camera.save();
-        camera.rotateX(degree);
+        camera.rotateY(45);
         canvas.translate(centerX, centerY);
         camera.applyToCanvas(canvas);
         canvas.translate(-centerX, -centerY);
         camera.restore();
 
-        canvas.drawBitmap(bitmap, x, y, paint);
+//        canvas.drawBitmap(bitmap, x, y, paint);
         canvas.restore();
     }
 }
